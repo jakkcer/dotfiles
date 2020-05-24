f3ed25cf7dfd62abc521eb08553b49c42423fd00
jakkcer <jakkcer.fivework@gmail.com>
2020-05-24 21:03:53 +0900

    update dotfilesLink

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,3 +1,9 @@
 #!bin/sh
+
+# vim files
 ln -sfv ~/.dotfiles/vim/.vimrc ~
 ln -sfv ~/.dotfiles/vim/rc/* ~/.vim/rc
+
+# zsh files
+ln -sfv ~/.dotfiles/zsh/.zshrc ~
+ln -sfv ~/.dotfiles/zsh/.zpreztorc ~
